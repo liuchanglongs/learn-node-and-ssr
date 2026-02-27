572e144e82eaae692915a33ab506edc16e8c8abc
lcl <2251086272@qq.com>
2026-02-27 14:54:24 +0800

    feat: 学习next的内置组件

---
+++ 05.Nextjs13/02-components-next/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import Script from "next/script";
+export default function Home() {
+  return (
+    <>
+      {/* 作用:方便我们做SEO 和 添加一个外部的资源 */}
+      <Head>
+        {/* html的标签 */}
+        <title>我是Title</title>
+        <meta name="description" content="lcl"></meta>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      </Head>
+
+      {/* 是给 home 首页的body 插入一个script标签 */}
+      {/* <Script src="https://www.baidu.com/"></Script> */}
+
+      <div>Hello World 111 </div>
+    </>
+  );
+}
