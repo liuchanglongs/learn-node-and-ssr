d6d79edbe84aa5e88b78b5a2cc605bccd2c86aec
lcl <2251086272@qq.com>
2026-02-27 14:57:43 +0800

    feat: 学习next组件目录整理

--- 05.Nextjs13/02-components-next/pages/index.tsx
+++ 05.Nextjs13/02-components-next/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Script from "next/script";
+import About from "./about";
 export default function Home() {
   return (
     <>
@@ -15,6 +16,7 @@ export default function Home() {
       {/* <Script src="https://www.baidu.com/"></Script> */}
 
       <div>Hello World 111 </div>
+      <About></About>
     </>
   );
 }
