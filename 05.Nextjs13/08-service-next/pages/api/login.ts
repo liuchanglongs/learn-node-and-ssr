df2c1d074dcd134b692bbf34fcc0d98e20caf454
lcl <2251086272@qq.com>
2026-03-01 14:15:10 +0800

    feat: 学习写接口

---
+++ 05.Nextjs13/08-service-next/pages/api/login.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+// /api/login -> POST
+export default function handlersdfsdf(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  console.log(req.url);
+  console.log(req.method);
+  console.log(req.query);
+  console.log(req.body);
+
+  if (req.method === "POST") {
+    const useInfo = {
+      name: "liujun",
+      age: 18,
+      token: "aabbcc",
+    };
+    res.status(200).send(useInfo);
+  } else {
+    res.status(405).end();
+  }
+}
